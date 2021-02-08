0d5deba92114660ed99501c5810472c5eb7b74ae
NB <test@example.com>
2021-02-08 01:12:19 -0500

    Adding drush alias, behat testing, drupal core update.

---
+++ tests/features/bootstrap/FailAid/Context/FailureContext.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace FailAid\Context;
+
+use Drupal\DrupalExtension\Context\RawDrupalContext;
+use Behat\Gherkin\Node\PyStringNode;
+use Behat\Gherkin\Node\TableNode;
+use Behat\Behat\Tester\Exception\PendingException;
+
+/**
+ * Defines application features from the specific context.
+ */
+class FailureContext extends RawDrupalContext {
+
+  /**
+   * Initializes context.
+   *
+   * Every scenario gets its own context instance.
+   * You can also pass arbitrary arguments to the
+   * context constructor through behat.yml.
+   */
+  public function __construct() {
+  }
+
+}
